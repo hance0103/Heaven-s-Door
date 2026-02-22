3e76257e09a9bfc5aafb607246088dbc96291847
hance0103 <83891076+hance0103@users.noreply.github.com>
2026-02-22 23:13:22 +0900

    캐릭터 약간 어두워졌던 오류 수정 및 보스 위치 설정

--- Assets/GamePlay/Enemy/EnemyController.cs
+++ Assets/GamePlay/Enemy/EnemyController.cs
@@ -1,17 +1,26 @@
 using System;
 using System.Collections.Generic;
 using GamePlay.GridMap;
+using Managers;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
 
+    [SerializeField] private Vector2Int middleNode;
+    [SerializeField] private List<Vector2Int> bossRange;
     [SerializeField] private List<Vector2Int> currentNode;
     public List<Vector2Int> CurrentNode => currentNode;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        transform.position = GridMath.NodeToWorld(
+            middleNode.x, 
+            middleNode.y, 
+            GameManager.Instance.gridManager.Origin, 
+            GameManager.Instance.gridManager.CellWorldSize);
         
+        ComputeBossNodes();
     }
 
     // Update is called once per frame
@@ -20,5 +29,13 @@ public class EnemyController : MonoBehaviour
         
     }
 
+    private void ComputeBossNodes()
+    {
+        currentNode.Add(middleNode);
+        foreach (var range in bossRange)
+        {
+            currentNode.Add(middleNode + range);
+        }
+    }
 
 }
